6b01a427ca1f966400532c5a246833fb41fdb0f3
Taffanie Ben <taffanie.ben@gmail.com>
2020-02-17 12:45:16 +0000

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+
+def ask
+end
+
+def answer
+
+  @user_answer = params[:question]
+
+  if params[:question].split("").include?("?")
+    @answer = "Silly question, get dressed and go to work!"
+  elsif params[:question] == "I am going to work"
+    @answer = "Great!"
+  else
+    @answer = "I don't care, get dressed and go to work!"
+  end
+end
+
+
+end
